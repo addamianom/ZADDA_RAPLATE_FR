b315ab8e27747914fc19c111da3b5b92e70b7878
Michele Addamiano <addamianom@gmail.com>
2026-08-16 10:48:58 +0000

    first commit

---
+++ zaddaraplate/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.esempirap.zaddaraplate.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
